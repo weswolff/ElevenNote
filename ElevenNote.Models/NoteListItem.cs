eac01b920db223943e98e809492a9b78679c5945
weswolff <weswolff@gmail.com>
2020-10-21 06:43:44 -0400

    NoteController Created

--- ElevenNote.Models/NoteListItem.cs
+++ ElevenNote.Models/NoteListItem.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ElevenNote.Models
 {
-    class NoteListItem
+    public class NoteListItem
     {
+        public int NoteId { get; set; }
+        public string Title { get; set; }
+        [Display(Name ="Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
     }
 }
